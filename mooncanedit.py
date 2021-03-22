a02861618b0b05e3a3d1e13ae3d8dcd7e4391d9b
QuackerDeezles <smarterboi@gmail.com>
2021-03-22 00:22:13 +0000

    Initial commit

---
+++ mooncanedit.py
@@ -0,0 +1,35 @@
+GOLD_TOKEN_EMOJI = '<:goldtoken:805527920512204821>'
+# Shop for Regular Token
+gdshopToken = """
+:coin: Easy-Insane Demon Verification - 20 tokens
+:coin: Legacy-Extended List Verification - 40 tokens 
+:coin: Main List Verification - 100 tokens
+:coin: Collab Layout Part - 15 tokens (+5 tokens for top quality)
+:coin: Collab Deco Part - 75 tokens (+25 tokens for top quality)
+:coin: Edited GD Video - 10 tokens per minute of final video
+"""
+
+# Shop for Gold Token 
+gdshopGoldToken = f""" 
+{GOLD_TOKEN_EMOJI} Skip a round or get 25 free points in a future competition
+{GOLD_TOKEN_EMOJI} 50 tokens (Can’t convert back to Gold Tokens)
+{GOLD_TOKEN_EMOJI} Collab Co-Host 
+{GOLD_TOKEN_EMOJI} Full Edited Gaming Video (Any game) 
+{GOLD_TOKEN_EMOJI} #announcements Advertisement (Collab, server, YT, Twitch, etc)
+
+"""
+
+list_desc = """
+**Lists**
+[60hz List Link](https://docs.google.com/document/d/17cPN0bUnEhkRf1Ar3XucxxnQeyzWEGmeVoUvGucuFF8/edit "By clicking this link, you will be transported to a Google Doc.")
+[Non-60hz List Link](https://docs.google.com/document/d/1-a0y3oCuRz1Iqyy88HojPHv6CVP9RMLddG2flr2LE9M/edit "By clicking this link, you will be transported to a Google Doc.")
+
+**Competition**
+[Competition Shop](https://docs.google.com/document/d/1WVRbiRy-ONbBGXDuDqBmxiYYg01EFKpw5PQdjA9tcnQ/edit  "By clicking this link, you will be transported to a Google Doc.")
+"""  
+
+competition_info = """
+There are no competitions right now.
+
+
+"""
