9c8dba83339a35a92568ce7f52d3ad83cd555fdc
QuackerDeezles <smarterboi@gmail.com>
2021-03-22 00:33:38 +0000

    stuff stop asking

---
+++ roleStuff.py
@@ -0,0 +1,125 @@
+roleParagraphs = """
+<@&757386030025277550> Me (?)
+@Server Admin Administrators of this server
+@Server Mod Moderators of this server
+@Trial Admin Those training to become an admin
+@Trial Mod Those training to become a mod
+@Developer Creators of Discord bots or other coding things
+@List Editor Those that will help add and place levels to the server 60hz and non-60hz lists
+
+@Destroying the Competition!! Anyone with 5+ competition wins
+@Competition Winner! Anyone with one more competition wins
+@Leaderboard Placer Anyone who's made it on to the top 3 in a competition
+
+@1000 Pointercrate Pts Those with 1000 or more points on Pointercrate (Proof req.)
+@500 Pointercrate Pts  Those with 500 or more points on Pointercrate (Proof req.)
+@250 Pointercrate Pts Those with 250 or more points on Pointercrate (Proof req.)
+@100 Pointercrate Pts  Those with 100 or more points on Pointercrate (Proof req.)
+
+@GD Mobile Player Those that play GD mostly on mobile
+@GD PC Player Those that play GD mostly on PC
+@Non-GD PC Player Those that don't play GD, but use mainly a computer for Discord/gaming
+@Non-GD Mobile Player Those that don't play GD, but use mainly a mobile device for Discord/gaming
+
+@Extreme Demon Anyone who's beaten an extreme demon (Proof req.)
+@Insane Demon  Anyone who's beaten an insane demon (Proof req.)
+@Hard Demon  Anyone who's beaten a hard demon
+@Medium Demon  Anyone who's beaten a medium demon
+@Easy Demon  Anyone who's beaten an easy demon
+@60hz GD Player Those that mainly play Geometry Dash on 60hz
+@GD Player (Not 60hz) Those that mainly play Geometry Dash on refresh rates other than 60hz
+
+@Muted Those that have violated message related rules and now can't post messages in this server
+@Bot Discord bots in this server
+@Member Any member in this server
+@Partner Someone who's server has partnered with this server
+@GD Creator Someone who has creator points in Geometry Dash (Proof req.)
+@Content Creator A content creator with 100+ Subscribers/Followers (Proof req.)
+@💎Server Booster Someone who has boosted the server pog. They also get 20 monthly tokens.
+@Donator :O Someone who has donated to the server in some way
+@Giveaway Winner Someone who has won a giveaway
+
+@Collab Current Verifier The current verifier for a collab in this sever
+@Collab Current Decorator A current decorator for a collab in this sever
+@Collab Current Layout Builder A current layout creator for a collab in this sever
+@Collab Current Playtester A current playtester for a collab in this sever
+@Current Collaborator Anyone participating in one of this servers' GD collabs
+
+@VIP A role given to a special few that get the following perks: 5 monthly tokens, #advertising at any level, giveaway priority, VIP on my Twitch streams, access to VIP and VIP+ only giveaways, and access to future plans for this server.
+@VIP+ The same as VIP, but... 5 more monthly tokens, a seasonal gold token, higher giveaway priority, access to VIP+ only giveaways, and access to future plans for my YT and Twitch.
+"""
+
+demonsROLE = """
+<@&775496621399081010>  Anyone who's beaten an extreme demon (Proof req.)
+<@&775496618627432471>  Anyone who's beaten an insane demon (Proof req.)
+<@&775496615649345547>  Anyone who's beaten a hard demon
+<@&775496613199347763>  Anyone who's beaten a medium demon
+<@&775496611521757184>  Anyone who's beaten an easy demon
+"""
+gdrolesROLE = """
+<@&757385356286099546> Those that mainly play Geometry Dash on 60hz
+<@&770711752500445214> Those that mainly play Geometry Dash on refresh rates other than 60hz
+<@&781606621997826058> Those that play GD mostly on mobile
+<@&782375600291315763> Those that play GD mostly on PC
+<@&788646246591627285> Those that don't play GD, but use mainly a computer for Discord/gaming
+<@&788646122292903937> Those that don't play GD, but use mainly a mobile device for Discord/gaming
+
+"""
+pointercrateROLE = """
+<@&775496607222726666> Those with 1000 or more points on Pointercrate (Proof req.)
+<@&775476912600252437>  Those with 500 or more points on Pointercrate (Proof req.)
+<@&775496609479262238> Those with 250 or more points on Pointercrate (Proof req.)
+<@&775476834351054910>  Those with 100 or more points on Pointercrate (Proof req.)
+"""
+competitionROLE = """
+<@&773343971875356683> Anyone with 5+ competition wins
+<@&773343869600923680> Anyone with one more competition wins
+<@&776653984076398602> Anyone who's made it on to the top 3 in a competition 
+
+"""
+collabROLE = """
+<@&783460940846071820> The current verifier for a collab in this sever
+<@&783461019485208578> A current decorator for a collab in this sever
+<@&783461072585359401> A current layout creator for a collab in this sever
+<@&783461121653997579> A current playtester for a collab in this sever
+<@&783473733867667517> Anyone participating in one of this servers' GD collabs
+
+
+"""
+
+moderationsROLE = """
+<@&757385793613332600> Administrators of this server
+<@&760317079466409994> Moderators of this server
+<@&779053250069397524> Those training to become an admin
+<@&779053192184987689> Those training to become a mod
+<@&758790944521715804> Creators of Discord bots or other coding things
+<@&757385714529730629> Those that will help add and place levels to the server 60hz and non-60hz lists
+
+"""
+
+perksROLE = """
+<@&783009905988468807> Someone who has boosted the server pog. They also get 20 monthly tokens.
+<@&796088132008804352> Someone who has donated to the server in some way
+<@&803486749253828658> A role given to a special few that get the following perks: 5 monthly tokens, #advertising at any level, giveaway priority, VIP on my Twitch streams, access to VIP and VIP+ only giveaways, and access to future plans for this server.
+<@&803486811480391680> The same as VIP, but... 5 more monthly tokens, a seasonal gold token, higher giveaway priority, access to VIP+ only giveaways, and access to future plans for my YT and Twitch.
+"""
+
+miscROLE = """
+<@&799680362271866940> Someone who has won a giveaway
+<@&770713196934987776> A content creator with 100+ Subscribers/Followers (Proof req.)
+<@&768909786192150619> Someone who has creator points in Geometry Dash (Proof req.)
+<@&758403393751613560> Someone who's server has partnered with this server
+"""
+
+memberRolesROLE = """
+<@&758028817280401550> Any member in this server
+<@&799003895531438101> - A member in the server that is level 5
+"""
+
+leaderboardToppers = """
+<@&809470260185006110> U-coin topper
+<@&809470000556540004> S-coin topper
+<@&809470707322191902> Demon topper
+<@&809470490275741746> Diamond Topper
+<@&809470388915404817> Star Topper
+"""
