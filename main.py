ee0929a0417a9ad5d1d99450a6215779f037a71b
QuackerDeezles <smarterboi@gmail.com>
2021-03-24 01:04:50 +0000

    e

--- main.py
+++ main.py
@@ -704,10 +704,10 @@ async def staff(ctx):
     for name in listofLM:
         listMods += str(f) + ". " + name + '\n'
         f += 1
-    for name in listofBoo:
-        serverBoosters += str(g) + ". " + name + ', '
-        g += 1
-    staffdesc = (serverowner + "\n1. [MoonFrost]((https://www.youtube.com/channel/UCxW9ilAhTGuhLvzIhgmSvyw))") + '\n\n' "2. **Server Co-Owner**\nQuackerDeezlesYT" + admin + '\n' + trialadmin + '\n' + listMods+ '\n' + mod + '\n' + trialmod + '\n' + devs + '\n' + serverBoosters
+   ## for name in listofBoo:
+    ##    serverBoosters += str(g) + ". " + name + ', '
+    ##    g += 1
+    staffdesc = (serverowner + "\n1. [MoonFrost]((https://www.youtube.com/channel/UCxW9ilAhTGuhLvzIhgmSvyw))") + '\n\n' "2. **Server Co-Owner**\nQuackerDeezlesYT\n\n" + admin + '\n' + trialadmin + '\n' + listMods+ '\n' + mod + '\n' + trialmod + '\n' + devs + '\n' + serverBoosters
     em = discord.Embed(title='Current Staff + Boosters',
                        description=staffdesc,
                        color=discord.Color.purple())
